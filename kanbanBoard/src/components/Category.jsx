e3141b5a96fc1d190e112880e19f69c20b66b5bf
Jaswanth <kurakulajaswanth5369@gmail.com>
2024-09-02 13:29:08 +0530

    Added drag and drop

--- kanbanBoard/src/components/Category.jsx
+++ kanbanBoard/src/components/Category.jsx
@@ -1,8 +1,18 @@
 import Task from "./Task.jsx";
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
+import { draggedTask } from "./Task.jsx";
+import { increment } from "../utils/toDoSlice.js";
+import { addItem as  addPeerReviewTask} from "../utils/peerReview.js";
+import { addItem as  addInProgressTask} from "../utils/inProgress.js";
+import { addItem as addDoneTask} from "../utils/done.js";
+import { addItem as addToDoTask } from "../utils/toDoSlice.js";
 
 function Category(props){
+    let textToFilter=useSelector((store)=>store.searchInput.searchInput);
+    let title;
     let tasks=[];
+    let length=useSelector((store)=>store.toDoTasks.counter);
+    let dispatch=useDispatch();
     if(props.category=="toDo"){
         title="To-Do";
         tasks=useSelector((store)=>store.toDoTasks.items);
@@ -16,13 +26,48 @@ function Category(props){
         title="Done"
         tasks=useSelector((store)=>store.doneTasks.items);
     }
-
+    
+    let filteredTasks=[];
+    for(let i=0;i<tasks.length;i++){
+        if(tasks[i] && tasks[i][1].toLowerCase().includes(textToFilter.toLowerCase())){
+            filteredTasks[filteredTasks.length]=tasks[i];
+        };
+    }
+    function handleDragOver(e){
+        e.preventDefault();
+    }
+    function handleDrop(e){
+        let dropTarget=e.target;
+        e.preventDefault();
+        while(!dropTarget.parentNode.classList.contains("category")){
+            dropTarget=dropTarget.parentNode;
+        }
+        if(dropTarget.parentNode.id=="toDo"){
+            let task=[length+1,draggedTask.current.children[0].children[0].innerHTML,draggedTask.current.children[1].children[0].innerHTML];
+            dispatch(addToDoTask(task));
+            dispatch(increment());
+        }else if(dropTarget.parentNode.id=="inProgress"){
+            let task=[length+1,draggedTask.current.children[0].children[0].innerHTML,draggedTask.current.children[1].children[0].innerHTML];
+            dispatch(addInProgressTask(task));
+            dispatch(increment());
+        }else if(dropTarget.parentNode.id=="peerReview"){
+            let task=[length+1,draggedTask.current.children[0].children[0].innerHTML,draggedTask.current.children[1].children[0].innerHTML];
+            dispatch(addPeerReviewTask(task));
+            dispatch(increment());
+        }else if(dropTarget.parentNode.id=="done"){
+            let task=[length+1,draggedTask.current.children[0].children[0].innerHTML,draggedTask.current.children[1].children[0].innerHTML];
+            dispatch(addDoneTask(task));
+            dispatch(increment());
+        }
+    }
     return(
         <div id={props.category} className="category">
             <h2>{title}</h2>
-            <div>
+            <div onDragOver={(e)=>{handleDragOver(e)}} onDrop={(e)=>handleDrop(e)}>
                 {filteredTasks.map((task)=>task && <Task key={task[0]} task={task} category={props.category}/>)}
             </div>
         </div>
     )
 }
+
+export default Category;
--- kanbanBoard/src/components/Task.jsx
+++ kanbanBoard/src/components/Task.jsx
@@ -1,8 +1,41 @@
+import { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { removeItem as removePeerReviewTask } from "../utils/peerReview.js";
+import { removeItem as removeInProgressTask } from "../utils/inProgress.js";
+import { removeItem as removeDoneTask} from "../utils/done.js";
+import { removeItem as removeToDoTask } from "../utils/toDoSlice.js";
+export let draggedTask;
 function Task(props){
+    let dispatch=useDispatch();
+    draggedTask=useRef([]);
+    function handleDrag(e){
+        draggedTask.current=e.target;
+    }
+    function handleDragEnd(e){
+        let dropElement=document.elementFromPoint(e.clientX,e.clientY);
+        
+        while(!(dropElement==document) && !dropElement.classList.contains("category")){
+            dropElement=dropElement.parentNode;
+        }
+        
+        if(!(dropElement==document) && dropElement.classList.contains("category")){
+            if(props.category=="toDo"){
+                dispatch(removeToDoTask(props.task));
+            }else if(props.category=="peerReview"){
+                dispatch(removePeerReviewTask(props.task));
+            }else if(props.category=="inProgress"){
+                dispatch(removeInProgressTask(props.task));
+            }else if(props.category=="done"){
+                dispatch(removeDoneTask(props.task));
+            }
+        }
+    }
     return(
-        <div className="taskCard" draggable>
+        <div className="taskCard" draggable onDrag={(e)=>handleDrag(e)} onDragEnd={(e)=>{handleDragEnd(e)}}>
             <div><div>{props.task[1]}</div></div>
             <div><div>{props.task[2]}</div></div>
         </div>
     )
 }
+
+export default Task;
